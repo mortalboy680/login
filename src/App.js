d338a503d73d4cb40d4f19a39479d0d2247e4a61
mortalboy680 <yago.gn007@gmail.com>
2025-05-05 18:40:14 +0000

    Add Login component integration to App layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App login_test">
       <Login />
     </div>
   );
